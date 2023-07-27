e919a258ac9bb122fb81912749c3960da517b64a
Teemi2106 <121306345+Teemi2106@users.noreply.github.com>
2023-07-27 18:38:19 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+const item1 = [
+  "Arabian Rug",
+  "8 cushion Pillows",
+  "Sequoia-made round table with crystal glass pane",
+  "4 Liquid-Light Lamps ",
+  "3 French-made Portriats",
+];
+
+const item2 = [
+  "Arabian Rug",
+  "4 cushion Pillows",
+  "Origami-based centre stool",
+  "2 Liquid-Light Lamp stand ",
+  "1 French-made Portriat",
+  "Featured Curtains",
+];
+const item3 = [
+  "Arabian Rug",
+  "8 cushion Pillows",
+  "Featured Glass centre table",
+  " 4 Home-featuredside-stools ",
+  "2 Liquid-Light Lamps ",
+  "3 French-made Portriats",
+  "Featured Artificial Ornaments",
+  "Featured Curtains",
+];
+const item4 = [
+  "Arabian Rug",
+  "5 cushion Pillows",
+  "Diamond-based centre stool",
+  " 2 Suite-featured side-stools ",
+  "1 Liquid-Light Lamp ",
+  "2 French-made Portriats",
+  "Featured Artificial Ornaments",
+  "Featured Curtains",
+];
+const item5 = [
+  "Arabian Rug",
+  "9 cushion Pillows",
+  "Family-Featured Centre Table with ornaments",
+  " 4 Family-featured side-stools ",
+  "2 Liquid-Light Lamp ",
+  "2 French-made Portriats",
+  "1 Family-Featured Portrait",
+  "Featured Artificial Ornaments",
+  "Featured Curtains",
+];
